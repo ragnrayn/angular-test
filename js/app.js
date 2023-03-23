b93ebb5ca6567b66caa8e1d500cac730700ca6da
Dima <mr.smith8263@gmail.com>
2023-03-24 00:31:47 +0100

    fix bugs

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const searchInput = document.querySelector(".search-form__input");
 const searchList = document.querySelector(".search-form__list");
-const searchForm = document.querySelector(".search-form");
+const searchLocation = document.querySelector(".search-form__location");
+const searchClose = document.querySelector(".search-form__close");
 
-searchInput.addEventListener("input", () => {
+searchInput.addEventListener("input", onInputHandler);
+
+searchClose.addEventListener("click", () => {
+  searchInput.value = "";
+  onInputHandler();
+});
+
+function onInputHandler() {
   if (searchInput.value.length > 0) {
     searchList.classList.add("active");
-    searchInput.classList.add("active");
+    searchLocation.classList.add("hidden");
+    searchClose.classList.add("visible");
   } else {
     searchList.classList.remove("active");
-    searchInput.classList.remove("active");
+    searchLocation.classList.remove("hidden");
+    searchClose.classList.remove("visible");
   }
-});
+}
