f8fda671a1f6a54bdefa6bebefb91b56af2c251d
Dima <mr.smith8263@gmail.com>
2023-04-03 22:46:26 +0200

    upd / meals card

--- js/search.js
+++ js/search.js
@@ -1,14 +1,27 @@
 function renderMeals() {
   const meals = JSON.parse(localStorage.getItem("meals"));
-  console.log(meals);
-  const promoWrapper = document.querySelector(".promo__wrapper");
+  const promoWrapper = document.querySelector(".promo__meals");
 
   const template = meals
     .map(({ title, price }) => {
       return `
-      <div>
-        <h3>${title}</h3>
-        <p>${price} грн</p>
+      <div class="meal">
+        <img class="meal__img" src="images/meals/burger.jpg" alt="burger" />
+        <p class="meal__restaurant">Ресторан</p>
+        <h3 class="meal__title">${title}</h3>
+        <p class="meal__descr">
+          Булочка солодова, овочі гриль (цукіні, баклажан, червона цибуля, болгарський перець), Моцарела, соус BBQ, салат Бостон, свіжі томати. Подається з картопляними діпами та салатом, але, на жаль, тимчасово без додаткових соусів
+        </p>
+        <div class="meal__footer">
+          <p class="meal__price">${price} ₴</p>
+          <button class="meal__btn">
+            <span class="icon-Button">
+              <span class="path1"></span>
+              <span class="path2"></span>
+            </span>
+          </button>
+        </div>
+        <div class="meal__delivery">50₴</div>
       </div>
     `;
     })
