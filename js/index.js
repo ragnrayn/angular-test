6ae63db8b596fe5a824b806520ab56929c3a02e3
Dima <mr.smith8263@gmail.com>
2023-04-06 21:19:06 +0200

    upd

--- js/index.js
+++ js/index.js
@@ -42,3 +42,17 @@ function searchMealsFromLocation() {
 }
 
 searchMealsFromLocation();
+
+let autocomplete;
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    {
+      types: ["establishment"],
+      componentRestrictions: { country: ["AU"] },
+      fields: ["place_id", "geometry", "name"],
+    }
+  );
+}
+initAutocomplete();
